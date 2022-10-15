7bd38592f8db9af01214d60364e4fbf8420149b0
eastzoo <dongju010@kakao.com>
2022-10-15 17:38:51 +0900

    feat : 유튜브 페이지 api 호출

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const Youtube = () => {
+  const [playlist, setPlaylist] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=UCBkyj16n2snkRg1BAzpovXQ&maxResults=50&key=AIzaSyAzHbmffJ970h-n6RdQF0Ndfu41VxSFZfc"
+      )
+      .then((res) => {
+        console.log(res);
+        setPlaylist(res.data.items);
+      })
+      .catch(() => {});
+  }, []);
+  console.log(playlist);
+
   return (
-      <div className='youtube'>
-        유튜브
-      </div>
+          <div className="container">
+            {playlist &&
+              playlist.map((i, idx) => {
+                return (
+                  <div className="playlist" key={idx}>
+                    <img src={i.snippet.thumbnails.high["url"]} alt="" />
+                    <Link to={"/playlist/" + i.id}>
+                      <h1>{i.snippet.localized["title"]}</h1>
+                    </Link>
+                    <p>{i.snippet.localized["description"]}</p>
+                  </div>
+                );
+              })}
+          </div>
   )
 }
 
